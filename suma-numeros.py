b392072aabf1134535a46685667ba6e6d7526844
Andres <dandres159@gmail.com>
2023-08-01 13:26:56 +0200

    Se subira los primeros documentos

---
+++ suma-numeros.py
@@ -0,0 +1,10 @@
+def sumar(num1, num2):
+
+    print("Aqui se realizara la suma de los numeros")
+    print("Despues se regresaran los numero para su correspondiente procesamiento")
+    return num1 + num2
+
+print(f"Esta es la suma de 10 y 15: " + str(sumar(10,15)) )
+
+
+
