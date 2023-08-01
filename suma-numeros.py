596ff5d019c6640d8e233cc2cc999e48fd07f101
Andres <dandres159@gmail.com>
2023-08-01 17:50:54 +0200

    Se subio la resta

--- suma-numeros.py
+++ suma-numeros.py
@@ -4,7 +4,14 @@ def sumar(num1, num2):
     print("Despues se regresaran los numero para su correspondiente procesamiento")
     return num1 + num2
 
-print(f"Esta es la suma de 10 y 15: " + str(sumar(10,15)) )
+print(f"Esta es la suma de 10 y 15: " + str(sumar(10,15)))
 
+
+def resta(num1, num2):
+    print("Aqui se realizara la resta de los numeros")
+    print("Despues se regresaran los numeros para ver que hacemos con ellos")
+    return num1 - num2
+
+print(f"Esta es la resta de 25 menos 15: " + str(resta(25,15)))
 
 
